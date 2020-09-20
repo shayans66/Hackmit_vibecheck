fb38ab6e381a843afd288d7a5ae7175d5cd25ab8
shayans66 <42shayan42@gmail.com>
2020-09-19 18:26:28 -0700

    background image

--- app.js
+++ app.js
@@ -6,12 +6,19 @@ submit.addEventListener('click', addElement);
 
 function addElement() {
   if (!clicked) {
+    // make new div
     const newDiv = document.createElement('div');
+    // create number
     let text = Math.floor(Math.random() * 100) + 1;
+    // create text content
     const newContent = document.createTextNode(text);
 
+    // append it to div
     newDiv.appendChild(newContent);
     const currentDiv = document.getElementById('div1');
+
+
+    // inserts content of 'newDiv' after 'currentDiv'
     document.body.insertBefore(newDiv, currentDiv);
     clicked = true;
   } else {
